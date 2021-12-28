a6be6efe93273030d3c70bf6e6dfa840c12921d7
Denis Kudinov <denis.kudinov@kaspersky.com>
2021-12-28 22:01:21 +0300

    Device inf impl

--- mdmserver/deviceinfo_command.go
+++ mdmserver/deviceinfo_command.go
@@ -12,9 +12,25 @@ type DeviceInformationCommandBody struct {
 	Queries     []string
 }
 
+type QueryResponses struct {
+	CarrierSettingsVersion string // Version of the carrier settings.
+	CurrentCarrierNetwork  string // Name of the current carrier network
+	CurrentMCC             string // Current Mobile Country Code (numeric string)
+	CurrentMNC             string // Current Mobile Network Code (numeric string)
+	ICCID                  string // The ICCID value.
+	IMEI                   string // The device IMEI number.
+	IsDataPreferred        bool   // If true, this subscription is preferred for data.
+	IsVoicePreferred       bool   // If true, this subscription is preferred for voice.
+	Label                  string // The label of this subscription.
+	LabelID                string // The UUID identifying this subscription (as a string)
+	MEID                   string // The device MEID number.
+	PhoneNumber            string // Raw phone number without punctuation, including country code
+	Slot                   string // Description of the slot containing the SIM representing this subscription.
+}
+
 type DeviceInformationCommandResponse struct {
 	CommandUUID    string
-	QueryResponses map[string]string
+	QueryResponses QueryResponses
 }
 
 func (command DeviceInformationCommand) UUID() string {
@@ -22,5 +38,20 @@ func (command DeviceInformationCommand) UUID() string {
 }
 
 func NewDeviceInformationCommand() DeviceInformationCommand {
-	return DeviceInformationCommand{CommandUUID: uuid.New().String(), Command: DeviceInformationCommandBody{RequestType: "DeviceInformation", Queries: []string{"DeviceName"}}}
+	queries := []string{
+		"CarrierSettingsVersion",
+		"CurrentCarrierNetwork",
+		"CurrentMCC",
+		"CurrentMNC",
+		"ICCID",
+		"IMEI",
+		"IsDataPreferred",
+		"IsVoicePreferred",
+		"Label",
+		"LabelID",
+		"MEID",
+		"PhoneNumber",
+		"Slot",
+	}
+	return DeviceInformationCommand{CommandUUID: uuid.New().String(), Command: DeviceInformationCommandBody{RequestType: "DeviceInformation", Queries: queries}}
 }
