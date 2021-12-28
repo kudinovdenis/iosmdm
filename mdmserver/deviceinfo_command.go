c4c2f2c127f3c3e1c556e0f43bd7b45c00d4e6df
Denis Kudinov <denis.kudinov@kaspersky.com>
2021-12-28 22:15:16 +0300

    Device inf impl

--- mdmserver/deviceinfo_command.go
+++ mdmserver/deviceinfo_command.go
@@ -12,7 +12,8 @@ type DeviceInformationCommandBody struct {
 	Queries     []string
 }
 
-type QueryResponses struct {
+type QueryResponseServiceSubscriptions struct {
+	// Service subscription properties
 	CarrierSettingsVersion string // Version of the carrier settings.
 	CurrentCarrierNetwork  string // Name of the current carrier network
 	CurrentMCC             string // Current Mobile Country Code (numeric string)
@@ -28,6 +29,29 @@ type QueryResponses struct {
 	Slot                   string // Description of the slot containing the SIM representing this subscription.
 }
 
+type QueryResponses struct {
+	// Network information
+	ICCID                    string   // The ICC identifier for the installed SIM card. skip (duplicate)
+	BluetoothMAC             string   // Bluetooth MAC address.
+	WiFiMAC                  string   // Wi-Fi MAC address.
+	EthernetMACs             []string // Ethernet MAC addresses. Availability: Available in iOS 7 and later.
+	EthernetMAC              string   // Primary Ethernet MAC address. Availability: Available in macOS v10.7 and later.
+	CurrentCarrierNetwork    string   // Name of the current carrier network. (duplicate)
+	SIMCarrierNetwork        string   // Name of the home carrier network. (Replaces SIMCarrierNetwork.)
+	SubscriberCarrierNetwork string   // Name of the home carrier network. (Replaces SIMCarrierNetwork.) Availability: Available in iOS 5.0 and later.
+	CarrierSettingsVersion   string   // Version of the currently-installed carrier settings file. (duplicate)
+	PhoneNumber              string   // Raw phone number without punctuation, including country code. (duplicate)
+	VoiceRoamingEnabled      bool     // The current setting of the Voice Roaming setting. This is only available on certain carriers. Availability: iOS 5.0 and later.
+	DataRoamingEnabled       bool     // The current setting of the Data Roaming setting.
+	IsRoaming                bool     // Returns whether the device is currently roaming. Availability: Available in iOS 4.2 and later. See note below.
+	PersonalHotspotEnabled   bool     // True if the Personal Hotspot feature is currently turned on. This value is available only with certain carriers. Availability: iOS 7.0 and later.
+	SubscriberMCC            string   // Home Mobile Country Code (numeric string). Availability: Available in iOS 4.2.6 and later.
+	SubscriberMNC            string   // Home Mobile Network Code (numeric string). Availability: Available in iOS 4.2.6 and later.
+	CurrentMCC               string   // Current Mobile Country Code (numeric string). (duplicate)
+	CurrentMNC               string   // Current Mobile Network Code (numeric string). (duplicate)
+	ServiceSubscriptions     []QueryResponseServiceSubscriptions
+}
+
 type DeviceInformationCommandResponse struct {
 	CommandUUID    string
 	QueryResponses QueryResponses
@@ -39,19 +63,26 @@ func (command DeviceInformationCommand) UUID() string {
 
 func NewDeviceInformationCommand() DeviceInformationCommand {
 	queries := []string{
-		"CarrierSettingsVersion",
+		// Network information
+		"ICCID",
+		"BluetoothMAC",
+		"WiFiMAC",
+		"EthernetMACs",
+		"EthernetMAC",
 		"CurrentCarrierNetwork",
+		"SIMCarrierNetwork",
+		"SubscriberCarrierNetwork",
+		"CarrierSettingsVersion",
+		"PhoneNumber",
+		"VoiceRoamingEnabled",
+		"DataRoamingEnabled",
+		"IsRoaming",
+		"PersonalHotspotEnabled",
+		"SubscriberMCC",
+		"SubscriberMNC",
 		"CurrentMCC",
 		"CurrentMNC",
-		"ICCID",
-		"IMEI",
-		"IsDataPreferred",
-		"IsVoicePreferred",
-		"Label",
-		"LabelID",
-		"MEID",
-		"PhoneNumber",
-		"Slot",
+		"ServiceSubscriptions",
 	}
 	return DeviceInformationCommand{CommandUUID: uuid.New().String(), Command: DeviceInformationCommandBody{RequestType: "DeviceInformation", Queries: queries}}
 }
