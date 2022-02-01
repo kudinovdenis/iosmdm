d84b2ab7e456347c47dcc1fbba934e78e3df7fa7
Denis Kudinov <denis.kudinov@kaspersky.com>
2022-02-01 12:44:44 +0300

    certbot integration

--- nginx/init-letsencrypt.sh
+++ nginx/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(m1553d.org www.m1553d.org)
+domains=(m1553d.com www.m1553d.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
